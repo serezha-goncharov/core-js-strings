7de4d78dd1cd5b935c65593b1fdc86746f678feb
Sergei Goncharov <blist.zheleza@gmail.com>
2024-10-31 10:39:48 +0700

    feat: solve encodeToRot13 subtask

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -501,8 +501,73 @@ function extractEmails(str) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  const strArray = str.split('');
+  const rot13CipherMap = {
+    A: 'N',
+    B: 'O',
+    C: 'P',
+    D: 'Q',
+    E: 'R',
+    F: 'S',
+    G: 'T',
+    H: 'U',
+    I: 'V',
+    J: 'W',
+    K: 'X',
+    L: 'Y',
+    M: 'Z',
+    N: 'A',
+    O: 'B',
+    P: 'C',
+    Q: 'D',
+    R: 'E',
+    S: 'F',
+    T: 'G',
+    U: 'H',
+    V: 'I',
+    W: 'J',
+    X: 'K',
+    Y: 'L',
+    Z: 'M',
+    a: 'n',
+    b: 'o',
+    c: 'p',
+    d: 'q',
+    e: 'r',
+    f: 's',
+    g: 't',
+    h: 'u',
+    i: 'v',
+    j: 'w',
+    k: 'x',
+    l: 'y',
+    m: 'z',
+    n: 'a',
+    o: 'b',
+    p: 'c',
+    q: 'd',
+    r: 'e',
+    s: 'f',
+    t: 'g',
+    u: 'h',
+    v: 'i',
+    w: 'j',
+    x: 'k',
+    y: 'l',
+    z: 'm',
+  };
+
+  for (let i = 0; i < strArray.length; i += 1) {
+    if (strArray[i] in rot13CipherMap) {
+      strArray[i] = rot13CipherMap[strArray[i]];
+    } else if (Object.values(rot13CipherMap).includes(strArray[i])) {
+      strArray[i] = Object.keys(rot13CipherMap).find(
+        (key) => rot13CipherMap[key] === strArray[i]
+      );
+    }
+  }
+  return strArray.join('');
 }
 
 /**
