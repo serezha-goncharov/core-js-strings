9e213eb162475e4cb3a7141a26b7a38209bfc032
Sergei Goncharov <blist.zheleza@gmail.com>
2024-10-31 10:27:20 +0700

    feat: solve removeFirstOccurrences subtask

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -146,8 +146,14 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const index = str.indexOf(value);
+  if (index === -1) {
+    return str;
+  }
+  return str
+    .slice(0, index)
+    .concat(str.slice(index + value.length, str.length));
 }
 
 /**
